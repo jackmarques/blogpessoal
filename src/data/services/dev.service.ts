d820fde46447e89a61c7e6afd7af503b6a6bb516
Jacqueline Cardeal <jacquelinecardeal@hotmail.com>
2026-02-05 17:32:05 -0300

    Correção database

--- src/data/services/dev.service.ts
+++ src/data/services/dev.service.ts
@@ -14,7 +14,7 @@ export class DevService implements TypeOrmOptionsFactory {
             port: 3306,
             username: 'root',
             password: 'root',
-            database: 'bd_blogpessoal',
+            database: 'db_blogpessoal',
             entities: [Postagem, Tema, Usuario],
             synchronize: true,
         }
